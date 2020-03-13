8c62c2d5cf5383b26eebb16d231b955c12742b23
cf_jijinliang <ji_jinliang2012@163.com>
2020-03-13 21:18:21 +0800

    Eureka集成的spring boot项目中

--- cloud-eureka-server-7001/src/main/java/org/jliang/apps/cloud/EurekaServerMain7001.java
+++ cloud-eureka-server-7001/src/main/java/org/jliang/apps/cloud/EurekaServerMain7001.java
@@ -3,23 +3,23 @@ package org.jliang.apps.cloud;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
- * 订单消费端
+ * Eureka服务器的注册中心
  *
  * @author Lenovo
  */
 @SpringBootApplication
+@EnableEurekaServer
 @Slf4j
-@EnableEurekaClient
-public class OrderConsumerMain80 {
+public class EurekaServerMain7001 {
     /**
-     * 主启动类
+     * 主入口方法
      *
      * @param args 命令行参数
      */
     public static void main(String[] args) {
-        SpringApplication.run(OrderConsumerMain80.class, args);
+        SpringApplication.run(EurekaServerMain7001.class, args);
     }
 }
--- cloud-provider-payment8001/src/main/java/org/jliang/apps/cloud/SpringCloudPaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/org/jliang/apps/cloud/SpringCloudPaymentMain8001.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.mybatis.spring.annotation.MapperScans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * payment 启动类
@@ -12,6 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @MapperScan("org.jliang.apps.cloud.dao")
+@EnableEurekaClient
 public class SpringCloudPaymentMain8001 {
     /**
      * spring boot 主启动类
