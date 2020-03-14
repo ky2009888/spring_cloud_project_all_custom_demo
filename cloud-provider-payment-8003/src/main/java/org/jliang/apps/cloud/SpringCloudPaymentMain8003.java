41c822d8b0adbe39060094ed66eb3d45b16c36e1
cf_jijinliang <ji_jinliang2012@163.com>
2020-03-14 22:22:45 +0800

    Spring cloud 集成zookeeper

---
+++ cloud-provider-payment-8003/src/main/java/org/jliang/apps/cloud/SpringCloudPaymentMain8003.java
@@ -0,0 +1,25 @@
+package org.jliang.apps.cloud;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 支付的服务端
+ *
+ * @author Lenovo
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@MapperScan(value = "org.jliang.apps.cloud.dao")
+public class SpringCloudPaymentMain8003 {
+    /**
+     * 主入口方法
+     *
+     * @param args 命令行参数
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudPaymentMain8003.class, args);
+    }
+}
