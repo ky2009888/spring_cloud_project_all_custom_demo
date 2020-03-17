14c5fefc799f0c8631f1c4ffb8e79f87a657a42f
cf_jijinliang <ji_jinliang2012@163.com>
2020-03-17 15:28:12 +0800

    新增openfeign的使用演示案例之详细日志打印

--- cloud-consumer-order80-openfeign/src/main/java/org/jliang/apps/cloud/config/ApplicationContext.java
+++ cloud-consumer-order80-openfeign/src/main/java/org/jliang/apps/cloud/config/ApplicationContext.java
@@ -1,5 +1,6 @@
 package org.jliang.apps.cloud.config;
 
+import feign.Logger;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,12 +15,18 @@ import org.springframework.web.client.RestTemplate;
 public class ApplicationContext {
     /**
      * 定义RestTemplate的实例
-     * @LoadBalanced 开启集群模式
+     *
      * @return RestTemplate
+     * @LoadBalanced 开启集群模式
      */
     @Bean
     @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public Logger.Level levelLogger() {
+        return Logger.Level.FULL;
     }
 }
